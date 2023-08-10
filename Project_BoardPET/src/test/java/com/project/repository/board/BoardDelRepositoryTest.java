dba658f7702e1547fbd3d7e4e16cc39ffc98c9fd
LeeHwangJi <fun4314@gmail.com>
2023-08-10 12:50:16 +0900

    게시글 및 댓글 기능구현 테스트 진행중

--- Project_BoardPET/src/test/java/com/project/repository/board/BoardDelRepositoryTest.java
+++ Project_BoardPET/src/test/java/com/project/repository/board/BoardDelRepositoryTest.java
@@ -1,12 +1,10 @@
 package com.project.repository.board;
 
-import static org.junit.Assert.*;
-
+import org.junit.Ignore;
 import org.junit.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import com.project.AppTest;
-import com.project.domain.board.BoardVO;
 import com.project.domain.board.boardDEL.BoardDelVO;
 
 import lombok.extern.log4j.Log4j;
@@ -17,9 +15,38 @@ public class BoardDelRepositoryTest extends AppTest {
 	@Autowired
 	BoardDelRepository boardDelRepository;
 	
+	@Autowired
+	BoardRepository boardRepository;
+	
+	@Ignore
 	@Test
-	public void boardDelInsertTest() { // 자유게시글 삭제 테이블 이동 및 해당 자유게시글 테이블 자료 삭제
-		boardDelRepository.boardDelInsert(1L);
+	public void boardDelReadTest() {
+		boardDelRepository.boardDelRead(1L);
+	}
+	
+	@Ignore
+	@Test
+	public void boardDelInsertTest() { // 자유게시글 삭제 테이블로1 이동 및 해당 자유게시글 테이블 자료 삭제
+		Long bno = boardDelRepository.boardDelInsert(3L); // 해당 데이터를 삭제 테이블로 이동
+		BoardDelVO boardDelRead = boardDelRepository.boardDelRead(bno); // 정상적인 삽입 확인
+		if(boardDelRead != null) {
+			boardRepository.boardDelete(bno);
+			log.info("삭제완료");
+			return;
+		}
+		log.info("정상적인 삽입이 이뤄지지 않음");
+	}
+	
+	@Test
+	public void boardDeleteTest() {
+		Long bno = 1L;
+		BoardDelVO boardDelRead = boardDelRepository.boardDelRead(bno); // 정상적인 삽입 확인
+		log.info("조회성공 : " + boardDelRead);
+		if(boardDelRead != null) {
+			boardRepository.boardDelete(bno);
+			log.info("삭제완료");
+		}
+		log.info("정상적인 삽입이 이뤄지지 않음");
 	}
 
 }
